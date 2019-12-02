fe515970644938723abcd1b5da5b8e977f681d6d
Kathia Mgwaba <mgwaba68@gmail.com>
2019-12-02 16:10:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ end
 def monopoly_with_fourth_tier
 	monopoly = {:railroads => {:pieces => 4,
 		:names  => {:reading_railroad => {"mortgage_value" => "$100"},
-	:pennsylvania_railroad => {},
+	:pennsylvania_railroad => {"mortgage_value" => "$200"},
 	:b_and_o_railroad => {},
 	:shortline => {}},
 		:rent_in_dollars => {:one_piece_owned => 25,
